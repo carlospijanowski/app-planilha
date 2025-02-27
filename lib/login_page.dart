785747b47670b181e52ed7c00e5534d5e91b895a
Cartaxo <carlos.pijanowski@gmail.com>
2025-02-27 13:44:56 -0300

    ajustes no projeto, separando e organizando

---
+++ lib/login_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart'; // Adjust this import based on your project structure.
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _loginController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _navigateToHome() {
+    if (_loginController.text.isNotEmpty && _passwordController.text.isNotEmpty) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Flutter Demo Home Page')));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Login and Password must not be empty!")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: _loginController,
+                  decoration: InputDecoration(
+                    hintText: 'Login',
+                    labelText: 'Login',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+                SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: _navigateToHome,
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(50), // makes the button taller
+                  ),
+                  child: const Text('Follow', style: TextStyle(fontSize: 18)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
